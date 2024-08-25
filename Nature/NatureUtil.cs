4e58bcb18abe48e530dd1d729435c746d309d498
rusted-coil <rusted.coil@gmail.com>
2024-08-25 10:02:12 +0900

    性格を追加

---
+++ Nature/NatureUtil.cs
@@ -0,0 +1,32 @@
+﻿using PKHeX.Core;
+
+namespace PKHeXUtilLib.Nature
+{
+    public static class NatureUtil
+    {
+        /// <summary>
+        /// 指定したIndexの性格名称を取得します。
+        /// </summary>
+        public static string GetNatureName(int natureIndex, string languageCode)
+        {
+            return Util.GetNaturesList(languageCode)[natureIndex];
+        }
+
+        /// <summary>
+        /// 性格名称から性格Indexを取得します。
+        /// <para> * 存在しない場合は-1を返します。</para>
+        /// </summary>
+        public static int GetNatureIndex(string natureName, string languageCode)
+        {
+            var natureList = Util.GetNaturesList(languageCode);
+            for (int i = 0; i < natureList.Length; ++i)
+            {
+                if (natureList[i] == natureName)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
